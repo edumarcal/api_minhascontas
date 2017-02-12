9b8c17aaaff17d1ad7bba25e088fc1e3dce9fe7f
edumarcal <edumarcalnet@gmail.com>
2017-02-12 10:26:39 -0300

    Corrigido o erro de inclusão de geração do token

--- app/controllers/user/saveUser.php
+++ app/controllers/user/saveUser.php
@@ -12,6 +12,6 @@
 */
 
 (@include_once(ROOT_DIR."/connection/connection.php")) or die(JSON_CONNECTION);
-(@include_once(ROOT_DIR."/generate_token_session.php")) or die(JSON_CONNECTION);
+(@include_once(ROOT_DIR."/validation/generate_token_session.php")) or die(JSON_VALIDATION);
 (@include_once(ROOT_DIR."/persistence/user/saveUser.php")) or die(JSON_PERSISTENCE);
 die(JSON_SUCESSO);
