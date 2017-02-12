39b04ea09a3c37cbb6bfd08daef6a654a99d8ae5
edumarcal <edumarcalnet@gmail.com>
2017-02-12 10:20:26 -0300

    teste connection database

--- app/validation/verify_token_session.php
+++ app/validation/verify_token_session.php
@@ -1,7 +1,8 @@
 <?php
 //Agradeço a Deus pelo dom do conhecimento
 //$token = apache_request_headers()['Token'] or die(JSON_TOKEN);
-$token = getallheaders()['Token'] or die(JSON_TOKEN);
+//$token = getallheaders()['Token'] or die(JSON_TOKEN);
+$token = $_SERVER['HTTP_TOKEN'] or die(JSON_TOKEN);
 
 $command = pg_query($banco, "SELECT name FROM users WHERE token = '$token'") or die('{"dbErro": "' . pg_last_error() . '"}');
 $name = pg_fetch_array($command)[0];
